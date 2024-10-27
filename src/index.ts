49e6abbf66c1f265ccdfc95239aab939646c617d
Slava <ja227@mail.ru>
2024-10-27 18:31:01 +0300

    Добавлены основные методы для сложения\вычитания, умножения\деления и возведения в степень

--- src/index.ts
+++ src/index.ts
@@ -18,4 +18,42 @@ class calc {
         this.history.push(operation);
         console.log(operation);
     }
+
+    add(value: number): number {
+        this.validateNumber(value);
+        this.memory += value;
+        this.logOperation(`Сложение: ${this.memory} + ${value} = ${this.memory}`);
+        return this.memory;
+    }
+
+    substract (value: number): number {
+        this.validateNumber(value);
+        this.memory -= value;
+        this.logOperation(`Вычитание: ${this.memory} - ${value} = ${this.memory}`);
+        return this.memory;
+    }
+
+    divide(value: number): number {
+        this.validateNumber(value);
+        if (value === 0) {
+            throw new Error("Ошибка: деление на ноль!");
+        }
+        this.memory /= value;
+        this.logOperation(`Деление: ${this.memory} / ${value} = ${this.memory}`);
+        return this.memory;
+    }
+
+    multiply (value: number): number {
+        this.validateNumber(value);
+        this.memory *= value;
+        this.logOperation(`Умножение: ${this.memory} * ${value} = ${this.memory}`);
+        return this.memory;
+    }
+
+    power(value: number): number {
+        this.validateNumber(value);
+        this.memory = Math.pow(this.memory, value);
+        this.logOperation(`Возведение в степень: ${this.memory} ^ ${value} = ${this.memory}`);
+        return this.memory;
+    }
 }
