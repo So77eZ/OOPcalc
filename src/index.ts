60c37230685a64af12a5e2b5848236b0980dbc00
Slava <ja227@mail.ru>
2024-10-27 18:19:51 +0300

    Добавлены базовые методы для отслеживания  действий и проверки на вводимое значение

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,15 @@ class calc {
         this.history = [];
         console.log(`Калькулятор инициализирован с памятью: ${this.memory}`);
     }
+
+    private validateNumber(value: number): void {
+        if (typeof value !== 'number') {
+            throw new Error("Ошибка: аргумент должен быть числом!");
+        }
+    }
+
+    private logOperation(operation: string): void {
+        this.history.push(operation);
+        console.log(operation);
+    }
 }
